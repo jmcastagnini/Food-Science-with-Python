e9436e4075bab6c6a0cb57672116cd03f7cca1dd
Juan Manuel Castagnini <jmcastagnini@protonmail.com>
2021-04-05 00:02:06 +0200

    categorical and numerical factors
    Added support for categorical and numerical factors for the anova_one_way function

--- Estadistica/analysis_variance.R
+++ Estadistica/analysis_variance.R
@@ -1,15 +1,38 @@
-analysis_variance <- function(df_r,analysis,treatment){
+analysis_variance <- function(df_r,analysis,treatment,factor){
 	library(agricolae)
     #A <- read.csv2(df,header = TRUE, dec = ".")  # Open the file and read the data 
     A <- df_r
-    A[ , c(analysis)] <- as.numeric(A[ , c(analysis)])  # Change the data type to numeric
-    m1 <- aov(as.formula(paste(analysis, '~',colnames(A)[treatment])), data = A)  # Make a linear model for the Post-hoc test
-    out1 <- HSD.test(m1,colnames(A)[treatment],console = FALSE)  # Make the analysis of variance by Groups=Treatments
+    
+    # Change the data type
+    A[ , c(analysis)] <- as.numeric(A[ , c(analysis)]) 
+    if (factor == 'numeric') {
+        A[ , c(treatment)] <- as.numeric(A[ , c(treatment)])
+    } else {
+        A[ , c(treatment)] <- as.factor(A[ , c(treatment)])
+    }
+    
+    # Make a linear model for the Post-hoc test
+    m1 <- aov(as.formula(paste(analysis, '~',colnames(A)[treatment])), data = A) 
+    
+    # Make the analysis of variance by Groups=Treatments
+    out1 <- HSD.test(m1,colnames(A)[treatment],console = FALSE)  
     df_treatments <- out1$groups
-    row_order <- row.names(df_treatments)  # Extract the name of the treatments to re-order=sort
-    tidy_df <- df_treatments[order(as.character(row_order)), ]  # Sort Data Frame with Base R (order Function)
+    
+    # Extract the name of the treatments to re-order=sort
+    row_order <- row.names(df_treatments)  
+    
+    # Sort Data Frame with Base R (order Function) and set the type of data (as.numeric or as.factor)
+    if (factor == 'numeric') {
+        tidy_df <- df_treatments[order(as.numeric(row_order)), ]
+    } else {
+        tidy_df <- df_treatments[order(as.character(row_order)), ]
+    }
+    
     mean_std_df <- out1$means[ , c(analysis, "std")]  # Data frame with medias and standard deviation
+    
     tidy_df[ , c(analysis)] <- paste(round((mean_std_df[ , c(analysis)]),digits = 3),"±",round((mean_std_df$std),digits = 3))
+    
     colnames(tidy_df)[colnames(tidy_df) == "groups"] <- paste0(analysis,'-','groups')
+    
     return(tidy_df)
 }
--- Estadistica/anova_one_way.py
+++ Estadistica/anova_one_way.py
@@ -4,16 +4,17 @@ import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri# Defining the R script and loading the instance in Python
 pandas2ri.activate()
 
-def anova1(df,treatment,col,sheet_name=0):
+def anova1(df,treatment,col,sheet_name=0,factor='categoric'):
     """df: dataframe with data as a string
        treatment: column number in R (starting in 1) where the treatment is located, usually is the 0 column
        col: initial column in Python (starting in 0) where result analysis are located
        sheet_name: only used for excel files, default value is 0 (First sheet of excel), Strings are used for sheet names. Integers are used in zero-indexed sheet positions
+       factor: by default is categoric; the other option is numeric
     """
     
     # Opening the Data frame
     extension = os.path.splitext(df)[1][1:]
-    if extension == 'xlsx':
+    if extension == 'xlsx' or extension == 'xls':
         df = pd.read_excel(io=df, sheet_name=sheet_name)
     elif extension =='csv':
         df = pd.read_csv(df, sep=';')
@@ -36,7 +37,7 @@ def anova1(df,treatment,col,sheet_name=0):
     # Statistical Results
     for analysis in column[col:len(column)]:
         #Invoking the R function and getting the result
-        df_result_r = analysis_variance_r(df_r, analysis,treatment)
+        df_result_r = analysis_variance_r(df_r, analysis,treatment,factor)
         #Converting it back to a pandas dataframe
         df_dict["{}".format(analysis)] = df_result_r
     return(df_dict)
